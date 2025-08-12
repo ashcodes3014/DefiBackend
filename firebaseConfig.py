41182bad1ee80c9c484089c6007b54292a8b673d
ashcodes3014 <aashishkumarak7065@gmail.com>
2025-08-13 00:47:10 +0530

    Update firebaseConfig.py

--- firebaseConfig.py
+++ firebaseConfig.py
@@ -1,8 +1,9 @@
 import firebase_admin
 from firebase_admin import db, firestore, credentials
-
+import os, json
+firebase_config = json.loads(os.environ["FIREBASE_CONFIG"])
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase.json")
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://defi-ai-assistant-default-rtdb.firebaseio.com/"
     })
